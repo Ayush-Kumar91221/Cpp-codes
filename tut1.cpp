28dc72d9f9e4e7033c57b9b56e5cc91ddb0a9262
Ayush <ayush.kumar.cse22@itbhu.ac.in>
2022-12-22 21:25:12 +0530

    added general syntax of cout without using namespace std

---
+++ tut1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello world";
+    std::cout<<"Hello world";//this is general syntax if you don't write 'using namespace std'
+    return 0;
+
+}
